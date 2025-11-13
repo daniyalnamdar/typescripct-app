1d3d155f526018816edef3348d7fd52ea3421d64
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2025-11-13 10:46:19 +0300

    adding props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import ListComponent from './components/ListComponent'
 
 function App() {
+   const items = ['PHP', 'JavaScript', 'Python', 'Swift']
+   const frameworks = ['Laravel', 'Symfony', 'Django', 'Dart']
+
+   const handleOnSelectItem = (item: string) => {
+      alert(item)
+   }
    return (
       <>
          <div className="flex flex-col">
-            <ListComponent />
-            <ListComponent />
+            <ListComponent
+               items={items}
+               heading="Programming Languages"
+               onSelectItem={handleOnSelectItem}
+            />
+            <ListComponent
+               items={frameworks}
+               heading="Frameworks"
+               onSelectItem={handleOnSelectItem}
+            />
          </div>
       </>
    )
--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 
-const ListComponent = () => {
-   const languages = ['PHP', 'JavaScript', 'Python', 'Swift']
+interface Props {
+   heading: string
+   items: string[]
+   onSelectItem: (item: string) => void
+}
+
+const ListComponent = ({ heading, items, onSelectItem }: Props) => {
    const [selectedItem, setSelectedItem] = useState(0)
    return (
       <>
          <div className="flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-bold mb-2">Programming Languages</h1>
+            <h1 className="text-2xl font-bold mb-2">{heading}</h1>
             <ul className="w-1/2 border border-grau-200 rounded-lg divide-y divide-gray-300 overflow-hidden">
-               {languages.map((language, index) => (
+               {items.map((item, index) => (
                   <li
                      className={
                         selectedItem === index
@@ -16,9 +21,11 @@ const ListComponent = () => {
                            : 'py-2 px-3'
                      }
                      key={index}
-                     onClick={() => setSelectedItem(index)}
+                     onClick={() => {
+                        setSelectedItem(index), onSelectItem(item)
+                     }}
                   >
-                     {language}
+                     {item}
                   </li>
                ))}
             </ul>
