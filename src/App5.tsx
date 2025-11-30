c7220771667694a8e1025bf97685ab309b39dbf6
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2025-11-30 18:54:12 +0300

    react icons 29 Done.

--- src/App5.tsx
+++ src/App5.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { IoIosSave } from 'react-icons/io'
+import stitch from './assets/1394966.jpg'
 
 function App5() {
    const tasks = ['Wake Up', 'Eat', 'Workout', 'Code', 'Sleep', 'Repeat']
-   const [active, setActive] = useState(0)
    return (
       <>
          <h1 className="text-2xl font-bold"> My Daily Tasks</h1>
@@ -14,13 +14,15 @@ function App5() {
             ))}
          </ul>
 
-         <ul className="list-style">
-            {tasks.map((task, index) => (
-               <li className="list-item-style" key={index}>
-                  {task}
-               </li>
-            ))}
-         </ul>
+         <div className="flex flex-col h-screen justify-center items-center">
+            <img src={stitch} alt="Sticth" className="w-1/3 mb-2" />
+            <button className="bg-gray-900 text-gray-300 px-8 py-9 rounded-xl text-xl">
+               <span className="flex gap-3 items-center">
+                  <IoIosSave className="text-3xl text-yellow-400" />
+                  Save
+               </span>
+            </button>
+         </div>
       </>
    )
 }
